de8aa954008fa47def779beca5e6dd6dec6bec75
Dallas James <iam@dallastjames.dev>
2024-05-16 11:52:52 -0500

    init to win it

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,33 @@
-import './ExploreContainer.css';
+import "./ExploreContainer.css";
+import {
+  CapacitorBarcodeScanner,
+  CapacitorBarcodeScannerTypeHint,
+} from "@capacitor/barcode-scanner";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  async function doScan() {
+    const result = await CapacitorBarcodeScanner.scanBarcode({
+      hint: CapacitorBarcodeScannerTypeHint.ALL,
+    });
+    console.log("Scan result: ", result);
+  }
+
   return (
     <div id="container">
       <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <p>
+        Start with Ionic{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+        or try a <button onClick={doScan}>Scan</button>
+      </p>
     </div>
   );
 };
